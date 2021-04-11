7b5d356fd56a8031fe973c6684765cdad066f74d
jordles113 <jordan.t.romero@gmail.com>
2021-04-11 13:18:25 -0500

    completes jsx exercise

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -4,12 +4,16 @@ import { Text, StyleSheet, View } from 'react-native';
 const ComponentsScreen = () => {
   const greeting = 'Hi there!';
   const greeting2 = <Text>Hello to you!</Text>;
+  const name = 'Jordan';
 
   return (
     <View>
       <Text style={styles.textStyle}>This is the ComponentsScreen</Text>
       <Text>{greeting}</Text>
       {greeting2}
+
+      <Text style={styles.headerStyle}>Getting started with React Native!</Text>
+      <Text style={styles.subHeaderStyle}>My name is {name}</Text>
     </View>
   );
 };
@@ -18,6 +22,14 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
     color: '#ff0000',
+  },
+  headerStyle: {
+    fontSize: 45,
+    color: 'blue',
+  },
+  subHeaderStyle: {
+    fontSize: 20,
+    color: 'teal',
   },
 });
 
