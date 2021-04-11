f07baadfd4bb7c1fd96597d90eaf00df225637f5
jordles113 <jordan.t.romero@gmail.com>
2021-04-11 12:52:46 -0500

    creates ComponentsScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Am Reloading?</Text>;
+  return <Text style={styles.text}>It's ALIVE</Text>;
 };
 
 const styles = StyleSheet.create({
