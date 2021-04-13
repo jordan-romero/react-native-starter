0791e2accd87b00790fea2af33a39ea8b0207fa2
jordles113 <jordan.t.romero@gmail.com>
2021-04-13 16:20:39 -0500

    creates a global variable to handle incremenation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [blue, setBlue] = useState(0);
@@ -10,19 +12,26 @@ const SquareScreen = () => {
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
         color='Red'
       />
       <ColorCounter
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
-        color='Blue'
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        color='Green'
       />
       <ColorCounter
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)}
-        color='Green'
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        color='Blue'
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+        }}
       />
     </View>
   );
