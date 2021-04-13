2b957a8c526c678170d23b8cff19b77b2f207d33
jordles113 <jordan.t.romero@gmail.com>
2021-04-13 18:25:26 -0500

    adds styles to input

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 const TextScreen = () => {
   return (
     <View>
-      <Text></Text>
+      <Text>Enter Name:</Text>
+      <TextInput style={styles.inputStyle} />
     </View>
   );
 };
 
 export default TextScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputStyle: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
