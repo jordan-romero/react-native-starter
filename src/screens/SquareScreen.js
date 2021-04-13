b365d0ba7617098efc4300dd66f6c7b02e8e0e0e
jordles113 <jordan.t.romero@gmail.com>
2021-04-13 16:35:28 -0500

    wires up switch statemnt to handle logic

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -10,12 +10,20 @@ const SquareScreen = () => {
   const [green, setGreen] = useState(0);
 
   const setColor = (color, change) => {
-    if (color === 'red') {
-      if (red + change > 255 || red + change > 0) {
+    switch (color) {
+      case 'red':
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
         return;
-      } else {
-        setRed(red + change);
-      }
+      case 'blue':
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+      case 'green':
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
     }
   };
 
