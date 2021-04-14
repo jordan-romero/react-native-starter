c47144c7a00b8ef90ab11fb4d1f1438a673a3110
jordles113 <jordan.t.romero@gmail.com>
2021-04-14 13:17:43 -0500

    using the boxobject model

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Hi from BoxScreen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Child 1</Text>
+      <Text style={styles.textStyle}>Child 2</Text>
+      <Text style={styles.textStyle}>Child 3</Text>
     </View>
   );
 };
 
 export default BoxScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+  },
+  textStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+});
